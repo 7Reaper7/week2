66617b1b2f35f752b0df98853f76f6025d3dcd9d
HP <apatroklus777666@gmail.com>
2022-12-15 23:06:01 +0500

    Lab Assignments

---
+++ PFWeek02LAB/LoopsTest.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int a,c=1;
+	string b;
+	while(c<198)
+	{
+	cout << "*";
+	c=c+1;
+	}
+	cout << endl;
+	cout << "Enter Your Name=>";
+	cin >> b;
+	for(a=1;a<20;a=a+1)
+	{
+	cout << "My name is " << b << endl << "I'm not a terrorist" << endl;
+	}
+}
